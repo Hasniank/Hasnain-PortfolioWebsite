56915dad164e05203b9984e5899210b79639d088
Hasniank <hk837488@gmail.com>
2023-08-21 14:53:33 +0500

    remove extra code

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -14,7 +14,6 @@ const ProfileSection = styled.section`
 const ProfileContainer = styled.div`
   width: 100%;
   height: 600px;
-  // background-color: #fff;
   border-radius: 8px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   display: flex;
@@ -44,7 +43,6 @@ font-family: monospace;
 letter-spacing: 7px!important;
 cursor: pointer;
 text-transform: uppercase;
-// padding: 64px;
   background: linear-gradient(to right, hsl(150, 0%, 100%) 0, hsl(0, 0%, 30%) 10%, hsl(150, 0%, 100%) 20%);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
